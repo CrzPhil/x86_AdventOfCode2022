8a6e467d725d2d137faf44519344899fa920820b
CrzPhil <philippos.g@me.com>
2022-12-11 14:09:41 +0200

    day9

--- day9/solve.py
+++ day9/solve.py
@@ -4,6 +4,33 @@ with open("input.txt", "r") as f:
 
 visited = set()
 
-grid = [[0 for i in range(100)] for i in range(100)]
+# init
+h = (0, 0)
+t = (0, 0)
 
-def isnear()
+def isNear(h, t):
+	return 0 <= abs(h[0]-t[0]) <= 1 and 0 <= abs(h[1]-t[1]) <= 1
+
+for line in lines:
+	d, v = line.split(' ')	# direction, value
+
+	for i in range(1, int(v)+1):
+		tmp = h
+
+		if d == 'U':
+			h = (h[0]+1, h[1])
+		elif d == 'R':
+			h = (h[0], h[1]+1)
+		elif d == 'D':
+			h = (h[0]-1, h[1])
+		else:
+			h = (h[0], h[1]-1)
+
+		if not isNear(h,t):
+			# If not diagonal
+			t = tmp
+
+		print(f"{d} {v} -> {t}")
+		visited.add(t)
+
+print(len(visited))
